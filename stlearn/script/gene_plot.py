2e4521be25ff2143d58164364fae57e9200b5bc3
Duy Pham <d.pham@imb19-014795.imb.uq.edu.au>
2020-12-16 18:01:21 +1000

    Update format and delete tmp files

--- stlearn/script/gene_plot.py
+++ stlearn/script/gene_plot.py
@@ -1,19 +1,43 @@
 from __future__ import division
 import numpy as np
 from PIL import Image
-from bokeh.plotting import figure, show, output_file, output_notebook, ColumnDataSource, curdoc
-from bokeh.models import BoxSelectTool, LassoSelectTool, CustomJS, Div,\
-                        LinearColorMapper, Slider, Panel, Select, AutocompleteInput, \
-                        ColorBar, TextInput,  BasicTicker, CrosshairTool,HoverTool
+from bokeh.plotting import (
+    figure,
+    show,
+    output_file,
+    output_notebook,
+    ColumnDataSource,
+    curdoc,
+)
+from bokeh.models import (
+    BoxSelectTool,
+    LassoSelectTool,
+    CustomJS,
+    Div,
+    LinearColorMapper,
+    Slider,
+    Panel,
+    Select,
+    AutocompleteInput,
+    ColorBar,
+    TextInput,
+    BasicTicker,
+    CrosshairTool,
+    HoverTool,
+)
 from bokeh.palettes import Spectral11
 from bokeh.layouts import column, row
 from collections import OrderedDict
+
 
 def make_gene_plot(data):
 
     library_id = list(data.uns["spatial"].keys())[0]
     # Open image, and make sure it's RGB*A*
-    image = (data.uns["spatial"][library_id]["images"][data.uns["spatial"]["use_quality"]]* 255).astype(np.uint8)
+    image = (
+        data.uns["spatial"][library_id]["images"][data.uns["spatial"]["use_quality"]]
+        * 255
+    ).astype(np.uint8)
 
     img_pillow = Image.fromarray(image).convert("RGBA")
 
@@ -26,107 +50,113 @@ def make_gene_plot(data):
     view = img.view(dtype=np.uint8).reshape((ydim, xdim, 4))
     # Copy the RGBA image into view, flipping it so it comes right-side up
     # with a lower-left origin
-    view[:,:,:] = np.flipud(np.asarray(img_pillow))
+    view[:, :, :] = np.flipud(np.asarray(img_pillow))
 
     # Display the 32-bit RGBA image
     dim = max(xdim, ydim)
-    
+
     gene_list = list(data.var_names)
 
     def make_fig():
 
+        fig = figure(
+            title="Gene plot",
+            x_range=(0, dim - 150),
+            y_range=(dim, 0),
+        )
 
-        fig = figure(title="Gene plot",
-                     x_range=(0,dim-150), y_range=(dim,0),
-                     )
-
-        colors = _gene_plot(data,"CumSum",[gene_select.value])
+        colors = _gene_plot(data, "CumSum", [gene_select.value])
 
         if threshold.value != "":
-            colors = colors[colors>float(threshold.value)]
-        
+            colors = colors[colors > float(threshold.value)]
+
         index_filter = colors.index
 
-        filter_obs  = data.obs.loc[index_filter]
+        filter_obs = data.obs.loc[index_filter]
         imagecol = filter_obs["imagecol"].values
-        imagerow = filter_obs["imagerow"].values 
+        imagerow = filter_obs["imagerow"].values
 
-        #coordinate = data.obs[["imagecol","imagerow"]].values
-        #x = coordinate[:,0]
-        #y = coordinate[:,1]
+        # coordinate = data.obs[["imagecol","imagerow"]].values
+        # x = coordinate[:,0]
+        # y = coordinate[:,1]
 
-        
-        #color = _gene_plot(data,"CumSum",[gene_list[0]])
+        # color = _gene_plot(data,"CumSum",[gene_list[0]])
         s1 = ColumnDataSource(data=dict(x=imagecol, y=imagerow, color=colors))
 
-        fig.image_rgba(image=[img], x=0, y=xdim, dw=ydim, dh=xdim,global_alpha=tissue_alpha.value)
+        fig.image_rgba(
+            image=[img], x=0, y=xdim, dw=ydim, dh=xdim, global_alpha=tissue_alpha.value
+        )
 
-        
-        color_mapper = LinearColorMapper(palette=Spectral11, low=min(s1.data['color']), high=max(s1.data['color']))
+        color_mapper = LinearColorMapper(
+            palette=Spectral11, low=min(s1.data["color"]), high=max(s1.data["color"])
+        )
 
-        fig.circle("x","y",
-                 color={'field': 'color', 'transform': color_mapper}, size=5, source=s1,
-                  fill_alpha =data_alpha.value, line_alpha=data_alpha.value)
+        fig.circle(
+            "x",
+            "y",
+            color={"field": "color", "transform": color_mapper},
+            size=5,
+            source=s1,
+            fill_alpha=data_alpha.value,
+            line_alpha=data_alpha.value,
+        )
 
-        color_bar = ColorBar(color_mapper=color_mapper, ticker=BasicTicker(),
-                         location=(10,0))
-        fig.add_layout(color_bar, 'right')
-
-        
+        color_bar = ColorBar(
+            color_mapper=color_mapper, ticker=BasicTicker(), location=(10, 0)
+        )
+        fig.add_layout(color_bar, "right")
 
         fig.toolbar.logo = None
         fig.xaxis.visible = None
         fig.yaxis.visible = None
         fig.xgrid.grid_line_color = None
         fig.ygrid.grid_line_color = None
-        fig.outline_line_alpha = 0 
+        fig.outline_line_alpha = 0
         fig.add_tools(LassoSelectTool())
         fig.add_tools(BoxSelectTool())
         fig.add_tools(HoverTool())
 
-        hover =fig.select(dict(type=HoverTool))
-        hover.tooltips = OrderedDict([
-            ("Spot", "$index"),
-            ("X location", "@x{1.11}"),
-            ("Y location", "@y{1.11}"),
-            ("Gene expression", "@color{1.11}"),
-        ])
-        
+        hover = fig.select(dict(type=HoverTool))
+        hover.tooltips = OrderedDict(
+            [
+                ("Spot", "$index"),
+                ("X location", "@x{1.11}"),
+                ("Y location", "@y{1.11}"),
+                ("Gene expression", "@color{1.11}"),
+            ]
+        )
+
         return fig
 
     def update_data(attrname, old, new):
         layout.children[1] = make_fig()
 
-
-
-    header = Div(text="""<h2>Gene plot: </h2>""",
-    width=400, height=50, sizing_mode='fixed')
+    header = Div(
+        text="""<h2>Gene plot: </h2>""", width=400, height=50, sizing_mode="fixed"
+    )
 
     data_alpha = Slider(title="Spot alpha", value=1.0, start=0, end=1.0, step=0.1)
-    data_alpha.on_change('value',update_data)
+    data_alpha.on_change("value", update_data)
 
     tissue_alpha = Slider(title="Tissue alpha", value=1.0, start=0, end=1.0, step=0.1)
-    tissue_alpha.on_change('value',update_data)
+    tissue_alpha.on_change("value", update_data)
 
     threshold = TextInput(title="Threshold:", value="")
-    threshold.on_change('value',update_data)
+    threshold.on_change("value", update_data)
 
-    gene_select = AutocompleteInput(title="Gene:", value=gene_list[0], completions=gene_list,
-        min_characters=1)
-    gene_select.on_change('value',update_data)
+    gene_select = AutocompleteInput(
+        title="Gene:", value=gene_list[0], completions=gene_list, min_characters=1
+    )
+    gene_select.on_change("value", update_data)
 
-    inputs = column(header,gene_select,data_alpha,tissue_alpha,threshold)
-
+    inputs = column(header, gene_select, data_alpha, tissue_alpha, threshold)
 
     layout = row(inputs, make_fig())
 
     # Make a tab with the layout
-    tab = Panel(child=layout,title="Gene plot")
+    tab = Panel(child=layout, title="Gene plot")
 
     return tab
-
-
-
 
 
 def _gene_plot(adata, method, genes):
@@ -134,13 +164,14 @@ def _gene_plot(adata, method, genes):
     # Gene plot option
 
     if len(genes) == 0:
-        raise ValueError('Genes shoule be provided, please input genes')
+        raise ValueError("Genes shoule be provided, please input genes")
 
     elif len(genes) == 1:
 
         if genes[0] not in adata.var.index:
             raise ValueError(
-                genes[0] + ' is not exist in the data, please try another gene')
+                genes[0] + " is not exist in the data, please try another gene"
+            )
 
         colors = adata[:, genes].to_df().iloc[:, -1]
 
@@ -150,18 +181,19 @@ def _gene_plot(adata, method, genes):
         for gene in genes:
             if gene not in adata.var.index:
                 genes.remove(gene)
-                warnings.warn("We removed " + gene +
-                              " because they not exist in the data")
+                warnings.warn(
+                    "We removed " + gene + " because they not exist in the data"
+                )
 
             if len(genes) == 0:
-                raise ValueError(
-                    'All provided genes are not exist in the data')
+                raise ValueError("All provided genes are not exist in the data")
 
         count_gene = adata[:, genes].to_df()
 
         if method is None:
             raise ValueError(
-                'Please provide method to combine genes by NaiveMean/NaiveSum/CumSum')
+                "Please provide method to combine genes by NaiveMean/NaiveSum/CumSum"
+            )
 
         if method == "NaiveMean":
             present_genes = (count_gene > 0).sum(axis=1) / len(genes)
--- stlearn/script/paga_plot.py
+++ stlearn/script/paga_plot.py
@@ -1,10 +1,30 @@
 from __future__ import division
 import numpy as np
 from PIL import Image
-from bokeh.plotting import figure, show, output_file, output_notebook, ColumnDataSource, curdoc
-from bokeh.models import BoxSelectTool, LassoSelectTool, CustomJS, CheckboxGroup,\
-                        LinearColorMapper, Slider, Panel, Select, AutocompleteInput, \
-                        IndexFilter, CDSView, Div, Paragraph, Span
+from bokeh.plotting import (
+    figure,
+    show,
+    output_file,
+    output_notebook,
+    ColumnDataSource,
+    curdoc,
+)
+from bokeh.models import (
+    BoxSelectTool,
+    LassoSelectTool,
+    CustomJS,
+    CheckboxGroup,
+    LinearColorMapper,
+    Slider,
+    Panel,
+    Select,
+    AutocompleteInput,
+    IndexFilter,
+    CDSView,
+    Div,
+    Paragraph,
+    Span,
+)
 from bokeh.palettes import Spectral11
 from bokeh.layouts import column, row
 import matplotlib
@@ -14,47 +34,47 @@ import stlearn as st
 import io
 import os
 
+
 def get_img_from_fig(fig, dpi=300):
     buf = io.BytesIO()
     from io import BytesIO
 
-    fig.savefig(buf, format="jpg", dpi=dpi, bbox_inches='tight',
-                pad_inches=0, transparent=False)
+    fig.savefig(
+        buf, format="jpg", dpi=dpi, bbox_inches="tight", pad_inches=0, transparent=False
+    )
     buf.seek(0)
     img_arr = np.frombuffer(buf.getvalue(), dtype=np.uint8)
     img = np.asarray(Image.open(BytesIO(img_arr)))
     buf.close()
-    #img = cv2.imdecode(img_arr, 1)
-    #img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+    # img = cv2.imdecode(img_arr, 1)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
 
     return img
 
 
+def make_paga_plot(data, use_label):
 
-def make_paga_plot(data,use_label):
+    # img_pillow =Image.fromarray(get_img_from_fig(fig)).convert("RGBA")
 
-    
-    #img_pillow =Image.fromarray(get_img_from_fig(fig)).convert("RGBA")
+    # xdim, ydim = img_pillow.size
 
-    #xdim, ydim = img_pillow.size
-
-    #print("Dimensions: ({xdim}, {ydim})".format(**locals()))
+    # print("Dimensions: ({xdim}, {ydim})".format(**locals()))
     # Create an array representation for the image `img`, and an 8-bit "4
     # layer/RGBA" version of it `view`.
-    #img = np.empty((ydim, xdim), dtype=np.uint32)
-    #view = img.view(dtype=np.uint8).reshape((ydim, xdim, 4))
+    # img = np.empty((ydim, xdim), dtype=np.uint32)
+    # view = img.view(dtype=np.uint8).reshape((ydim, xdim, 4))
     # Copy the RGBA image into view, flipping it so it comes right-side up
     # with a lower-left origin
-    #view[:,:,:] = np.flipud(np.asarray(img_pillow))
+    # view[:,:,:] = np.flipud(np.asarray(img_pillow))
 
     # Display the 32-bit RGBA image
-    #dim = max(xdim, ydim)
-    
-    header = Div(text="""<h2>PAGA plot: </h2>""",
-    width=400, height=30, sizing_mode='fixed')
- 
-    fig, a = plt.subplots()
+    # dim = max(xdim, ydim)
 
+    header = Div(
+        text="""<h2>PAGA plot: </h2>""", width=400, height=30, sizing_mode="fixed"
+    )
+
+    fig, a = plt.subplots()
 
     try:
         del data.obsm["X_diffmap"]
@@ -65,46 +85,54 @@ def make_paga_plot(data,use_label):
     except:
         pass
 
-    data.uns["iroot"] = np.flatnonzero(data.obs[use_label]  == str(0))[0]
-    st.spatial.trajectory.pseudotime(data,eps=100)
+    data.uns["iroot"] = np.flatnonzero(data.obs[use_label] == str(0))[0]
+    st.spatial.trajectory.pseudotime(data, eps=100)
 
-    data.uns[use_label+"_colors"] = data.uns["tmp_color"]
+    data.uns[use_label + "_colors"] = data.uns["tmp_color"]
 
-    scanpy.pl.paga(data, color=use_label,ax=a,show=False,edge_width_scale=3.5, 
-        node_size_scale=3.5)
-    a.axis('off')
+    scanpy.pl.paga(
+        data,
+        color=use_label,
+        ax=a,
+        show=False,
+        edge_width_scale=3.5,
+        node_size_scale=3.5,
+    )
+    a.axis("off")
     fig.set_size_inches(5, 5)
 
-    filelist = [ f for f in os.listdir("stlearn/static/") if f.endswith(".png") ]
+    filelist = [f for f in os.listdir("stlearn/static/") if f.endswith(".png")]
     for f in filelist:
         os.remove(os.path.join("stlearn/static/", f))
 
-
-    fig.savefig("stlearn/static/tmp.png", dpi=150,
-                bbox_inches='tight', pad_inches=0)
+    fig.savefig("stlearn/static/tmp.png", dpi=150, bbox_inches="tight", pad_inches=0)
 
     paga = Div(text="""<img src='stlearn/static/tmp.png' style='float: left;'>""")
 
     fig2, a2 = plt.subplots()
 
-    scanpy.tl.draw_graph(data, init_pos='paga')
+    scanpy.tl.draw_graph(data, init_pos="paga")
     data.uns["louvain_colors"] = data.uns["tmp_color"]
 
-    scanpy.pl.draw_graph(data, color="louvain", legend_loc='on data',ax=a2,show=False,
-        )
-    a2.axis('off')
-    fig2.set_size_inches(5,5)
+    scanpy.pl.draw_graph(
+        data,
+        color="louvain",
+        legend_loc="on data",
+        ax=a2,
+        show=False,
+    )
+    a2.axis("off")
+    fig2.set_size_inches(5, 5)
 
-    fig2.savefig("stlearn/static/tmp2.png", dpi=150,
-                bbox_inches='tight', pad_inches=0)
+    fig2.savefig("stlearn/static/tmp2.png", dpi=150, bbox_inches="tight", pad_inches=0)
 
     fa2 = Div(text="""<img src='stlearn/static/tmp2.png'>""")
 
-    #if len(inputs.children) == 2:
+    # if len(inputs.children) == 2:
     #    inputs.children.pop()
 
-    figures = row(paga,fa2)
+    figures = row(paga, fa2)
 
-    inputs = column(header,figures)
+    inputs = column(header, figures)
 
     return inputs
